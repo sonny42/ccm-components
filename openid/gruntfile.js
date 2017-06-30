b2de0605fcf6e0b17498662db4f00b25b593f7b1
Philipp Sonnenberger <philipp.sonnenberger@gmail.com>
2017-06-30 17:31:31 +0200

    live reload with grunt

--- openid/gruntfile.js
+++ openid/gruntfile.js
@@ -1,5 +1,23 @@
 module.exports = function (grunt) {
-    grunt.registerTask("default", function () {
-        grunt.log.writeln("Hello World");
-    })
+
+    grunt.loadNpmTasks('grunt-serve');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        //serve files on localhost:9000
+        serve: {
+            options: {
+                port: 9000
+            }
+        },
+        //live reload on source change
+        watch:{
+            files:['*.js', '*.html', '*.json', '*.css'], //include json files to enable reload on ccm template change
+            options: {
+                livereload: 9001
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['watch']);
 }
